b20a28437dd498153bf701e3463fcd229a164051
XINYI-DOU <shaqiumeiying@gmail.com>
2024-05-31 22:27:48 -0700

    adaptation for vercel deployment

--- server.js
+++ server.js
@@ -1,20 +1,16 @@
- require('dotenv').config();
 const express = require("express");
-const router = express.Router();
 const cors = require("cors");
 const nodemailer = require("nodemailer");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use("/", router);
-app.listen(5000, () => console.log("Server Running"));
 
 const contactEmail = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: "shaqiumeiying@gmail.com",
-    pass: "jpsh gpyr yszk ztxn"
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
   },
 });
 
@@ -26,14 +22,14 @@ contactEmail.verify((error) => {
   }
 });
 
-router.post("/contact", (req, res) => {
+app.post("/api/contact", (req, res) => {
   const name = req.body.firstName + " " + req.body.lastName;
   const email = req.body.email;
   const message = req.body.message;
   const phone = req.body.phone;
   const mail = {
     from: name,
-    to: "shaqiumeiying@gmail.com",
+    to: process.env.EMAIL_USER,
     subject: "Contact Form Submission - Portfolio",
     html: `<p>Name: ${name}</p>
            <p>Email: ${email}</p>
@@ -48,3 +44,5 @@ router.post("/contact", (req, res) => {
     }
   });
 });
+
+module.exports = app;

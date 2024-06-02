63a43e0176266eb32c901946d13563e06e2659da
XINYI-DOU <shaqiumeiying@gmail.com>
2024-06-01 17:34:03 -0700

    starcanvas hide when on phone

--- src/component/Stars.js
+++ src/component/Stars.js
@@ -1,4 +1,4 @@
-import { useState, useRef, Suspense } from "react";
+import { useEffect, useState, useRef, Suspense } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { Points, PointMaterial, Preload } from "@react-three/drei";
 import * as random from "maath/random/dist/maath-random.esm";
@@ -6,7 +6,8 @@ import * as random from "maath/random/dist/maath-random.esm";
 const Stars = (props) => {
   const ref = useRef();
   const [sphere] = useState(() =>
-  random.inSphere(new Float32Array(5001), { radius: 1.2 }));
+    random.inSphere(new Float32Array(5001), { radius: 1.2 })
+  );
 
   useFrame((state, delta) => {
     ref.current.rotation.x += delta / 10;
@@ -15,10 +16,16 @@ const Stars = (props) => {
 
   return (
     <group rotation={[0, 0, Math.PI / 4]}>
-      <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
+      <Points
+        ref={ref}
+        positions={sphere}
+        stride={3}
+        frustumCulled
+        {...props}
+      >
         <PointMaterial
           transparent
-          color='#f272c8'
+          color="#f272c8"
           size={0.002}
           sizeAttenuation={true}
           depthWrite={false}
@@ -29,18 +36,32 @@ const Stars = (props) => {
 };
 
 const StarsCanvas = () => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
-    <div className='stars-canvas-container'>
-      <Canvas camera={{ position: [0, 0, 1] }}>
-        <Suspense fallback={null}>
-          <Stars />
-        </Suspense>
-        <Preload all />
-      </Canvas>
+    <div className="stars-canvas-container">
+      {windowWidth >= 990 && (
+        <Canvas camera={{ position: [0, 0, 1] }}>
+          <Suspense fallback={null}>
+            <Stars />
+          </Suspense>
+          <Preload all />
+        </Canvas>
+      )}
     </div>
   );
 };
-
-
 
 export default StarsCanvas;

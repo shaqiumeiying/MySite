9fc52a1dd27218d8a87f954638b3c0e80f410e30
XINYI-DOU <shaqiumeiying@gmail.com>
2024-06-02 14:33:42 -0700

    minor fix

--- src/component/LoadGLTF.js
+++ src/component/LoadGLTF.js
@@ -2,6 +2,7 @@ import { Suspense, useEffect, useState, lazy } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls, Stage, PerspectiveCamera } from '@react-three/drei';
 import styled from 'styled-components';
+import StarsCanvas from './StarCanvas';
 
 const DesktopModel = lazy(() => import('./Compressed-emb'));
 const MobileModel = lazy(() => import('./Starsemb'));
@@ -32,30 +33,14 @@ const InstructionText = styled.div`
   z-index: 1;
 `;
 
-// const PaddingContainer = styled.div`
-//   height: 60px;
-//   width: 100%;
-// `;
-
 const LoadGLTF = () => {
   const [loading, setLoading] = useState(true);
-  // eslint-disable-next-line
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
   const [modelPosition, setModelPosition] = useState([0, 0, 0]);
-
-  useEffect(() => {
-    const handleResize = () => setWindowWidth(window.innerWidth);
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
-  }, []);
 
   useEffect(() => {
     const timer = setTimeout(() => setLoading(false), 2000);
     return () => clearTimeout(timer);
   }, []);
-
-  const handleScroll = () => {
-  };
 
   const updateModelPosition = (position) => {
     setModelPosition(position);
@@ -64,13 +49,14 @@ const LoadGLTF = () => {
   const isMobileDevice = /Mobi|Android/i.test(navigator.userAgent);
 
   return (
-    <Container onScroll={handleScroll}>
-      <InstructionText>Drag to rotate, scroll to zoom</InstructionText>
+    <Container>
+      {!isMobileDevice && <InstructionText>Drag to rotate, pinch/scroll to zoom</InstructionText>}
       {loading && <LoadingText>Loading 3D Model...</LoadingText>}
       <Canvas>
         <ambientLight intensity={0.4} />
         <PerspectiveCamera makeDefault fov={75} position={[0, 2, 13]} />
         <OrbitControls
+          autoRotate={isMobileDevice}
           position={[0, 2, 13]}
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
@@ -78,28 +64,15 @@ const LoadGLTF = () => {
           onUpdate={(state) => updateModelPosition(state.target.toArray())}
         />
         <Suspense fallback={null}>
-          
-            {isMobileDevice ? (
-              <Stage
-              environment="park"
-              intensity={1}
-              scale={[1, 1, 1]}
-              position={[modelPosition]}
-              shadows={false}
-            >
+          {isMobileDevice ? (
+            <Stage environment="park" intensity={1} scale={[1, 1, 1]} position={modelPosition} shadows={false}>
               <MobileModel onLoaded={() => setLoading(false)} />
-              </Stage>
-            ) : (
-              <Stage
-              environment="sunset"
-              intensity={1}
-              scale={[1, 1, 1]}
-              position={modelPosition}
-            >
+            </Stage>
+          ) : (
+            <Stage environment="sunset" intensity={1} scale={[1, 1, 1]} position={modelPosition}>
               <DesktopModel onLoaded={() => setLoading(false)} />
-              </Stage>
-            )}
-        
+            </Stage>
+          )}
         </Suspense>
       </Canvas>
     </Container>
@@ -107,3 +80,4 @@ const LoadGLTF = () => {
 };
 
 export default LoadGLTF;
+
